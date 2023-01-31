b4a75e0aa8c381a75481127447fda5ccc733a877
Igor Ermentraut <ie@efsa.io>
2023-01-31 23:08:01 +0100

    forgot to fix path in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
-if [ "$(ls -A /code)" ]; then
-    /usr/local/bin/php /application/vendor/bin/ecs check -c "$(pwd)/easy-coding-standard.php" --fix -- /code
+if [ "$(ls -A /src)" ]; then
+    /usr/local/bin/php /application/vendor/bin/ecs check -c "$(pwd)/easy-coding-standard.php" --fix -- /src
 else
     echo "No files to check. Skipping."
 fi
