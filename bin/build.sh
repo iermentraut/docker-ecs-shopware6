475b3dab84fe42242e4e3a683ab1e94c422b695b
Igor Ermentraut <35839308+iermentraut@users.noreply.github.com>
2023-01-31 23:17:29 +0100

    Delete bin directory

--- bin/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t shopware-ecs-docker:6.4-php8.0 --build-arg SHOPWARE_VERSION=6.4 --build-arg PHP_VERSION=8.0 .
-docker build -t shopware-ecs-docker:6.3-php8.0 --build-arg SHOPWARE_VERSION=6.3 --build-arg PHP_VERSION=8.0 .
