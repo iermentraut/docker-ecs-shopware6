13be2b81220ee8c8f03fbb2226bfab3b00144ba5
Igor Ermentraut <ie@efsa.io>
2023-01-31 12:53:52 +0100

    skip ecs if /code is empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 
 if [ "$(ls -A /code)" ]; then
     /usr/local/bin/php /application/vendor/bin/ecs check -c "$(pwd)/easy-coding-standard.php" --fix -- /code
+else
+    echo "No files to check. Skipping."
 fi
