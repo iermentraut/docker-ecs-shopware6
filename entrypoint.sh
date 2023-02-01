008ea8ee874b276aa8647841780e068b86f5ff59
Igor Ermentraut <ie@efsa.io>
2023-02-01 17:23:25 +0100

    ecs should work now

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,15 @@
 
 if [ "$(ls -A /src)" ]; then
     if [ ! -e /src/ecs.php ] && [ ! -e /src/easy-coding-standard.php ]; then
-        echo "[INFO] No ecs.php or easy-coding-standard.php found. Using default configuration \"https://raw.githubusercontent.com/shopware/production/${SHOPWARE_VERSION}/easy-coding-standard.php.\""
-
-        /usr/local/bin/php /application/vendor/bin/ecs check --fix platform/src --config /application/ecs.php
+        /usr/local/bin/php /application/vendor/bin/ecs check --fix --config /application/ecs.php -- /src
     fi
 
     if [ -e /src/easy-coding-standard.php ]; then
-        /usr/local/bin/php /application/vendor/bin/ecs check -c /application/easy-coding-standard.php --fix -- /src
+        /usr/local/bin/php /application/vendor/bin/ecs check --fix --config /application/easy-coding-standard.php -- /src
     fi
 
     if [ -e /src/ecs.php ]; then
-        /usr/local/bin/php /application/vendor/bin/ecs check -c /application/ecs.php --fix -- /src
+        /usr/local/bin/php /application/vendor/bin/ecs check --fix --config /application/ecs.php -- /src
     fi
 else
     echo "Directory /src is empty. Skipping."
